a6be5371f48aa16c7acebbe3a29568294261b054
stygian <stygian@styx.hell>
2018-10-05 19:53:22 +0200

    Refactorings

---
+++ com.test.e4.application/src/com/test/e4/application/model/TodoService.java
@@ -0,0 +1,18 @@
+package com.test.e4.application.model;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Consumer;
+
+public interface TodoService {
+
+	void getTodos(Consumer<List<Todo>> todosConsumer);
+
+	boolean saveTodo(Todo newTodo);
+	
+	Optional<Todo> getTodo(int id);
+	
+	boolean deleteTodo(int id);
+	
+	void printTodos();
+}
