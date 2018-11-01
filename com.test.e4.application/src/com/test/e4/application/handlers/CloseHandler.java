2c33f2ad6ce60d32f4c95e237e70054d1e894033
stygian <stygian@styx.hell>
2018-11-01 20:11:42 +0100

    SingleSourcing prepared

--- com.test.e4.application/src/com/test/e4/application/handlers/CloseHandler.java
+++ com.test.e4.application/src/com/test/e4/application/handlers/CloseHandler.java
@@ -3,12 +3,16 @@ package com.test.e4.application.handlers;
 import java.util.List;
 
 import org.eclipse.e4.core.di.annotations.Execute;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.test.e4.application.Context;
 import com.test.e4.application.model.Project;
 
 public class CloseHandler {
 
+	private static final Logger logger = LoggerFactory.getLogger(CloseHandler.class);
+	
 	@Execute
 	public void execute() {
 		List<Project> projects = Context.getInstance().projects;
@@ -16,6 +20,7 @@ public class CloseHandler {
 			Project project = projects.get(projects.size() - 1);
 			project.close();
 			projects.remove(project);
+			logger.info("...project removed");
 		}
 	}
 
--- com.test.e4.application/src/com/test/e4/application/handlers/OpenHandler.java
+++ com.test.e4.application/src/com/test/e4/application/handlers/OpenHandler.java
@@ -1,17 +1,22 @@
 package com.test.e4.application.handlers;
 
 import org.eclipse.e4.core.di.annotations.Execute;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.test.e4.application.Context;
 import com.test.e4.application.model.Project;
 
 public class OpenHandler {
 
+	private static final Logger logger = LoggerFactory.getLogger(OpenHandler.class);
+
 	@Execute
 	public void execute() {
 		Project project = new Project();
 		project.open();
 		Context.getInstance().projects.add(project);
+		logger.info("...project added");
 	}
 	
 }
